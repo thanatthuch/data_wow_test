844e918caadc212989f855b1a6b755badb1c41d1
Thanatthuch Kamsneg <52025403+thanatthuch@users.noreply.github.com>
2023-12-28 16:04:43 +0700

    Update sample_ingest.py

--- dags/sample_ingest.py
+++ dags/sample_ingest.py
@@ -25,7 +25,7 @@ targetSchema    : dict = {
 targetPartition :str = "create_at"
 
 
-connection : str = "postgres_default"
+
 
 defaultArgs : dict = {
     'owner': 'airflow',
@@ -33,7 +33,7 @@ defaultArgs : dict = {
     'email_on_failure': False,
     'email_on_retry': False,
 }
-pg_hook_load = PostgresHook(postgres_conn_id=connection)
+
 
 
 
@@ -46,63 +46,8 @@ dag = DAG(
     schedule_interval=None
 )
 
-#################################################################################### TABLE Creation
-createCommand=f"""CREATE TABLE IF NOT EXISTS {targetTable} (
-    department_name VARCHAR(32),
-    sensor_serial VARCHAR(64),
-    create_at TIMESTAMP,
-    product_name VARCHAR(16),
-    product_expire TIMESTAMP)
-    PARTITION BY RANGE (create_at);
-    
-    CREATE TABLE IF NOT EXISTS {targetTable}_01 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-01 00:00:00') TO ('2023-01-02 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_02 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-02 00:00:00') TO ('2023-01-03 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_03 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-03 00:00:00') TO ('2023-01-04 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_04 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-04 00:00:00') TO ('2023-01-05 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_05 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-05 00:00:00') TO ('2023-01-06 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_06 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-06 00:00:00') TO ('2023-01-07 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_07 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-07 00:00:00') TO ('2023-01-08 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_08 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-08 00:00:00') TO ('2023-01-09 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_09 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-09 00:00:00') TO ('2023-01-10 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_10 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-10 00:00:00') TO ('2023-01-11 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_11 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-11 00:00:00') TO ('2023-01-12 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_12 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-12 00:00:00') TO ('2023-01-13 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_13 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-13 00:00:00') TO ('2023-01-14 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_14 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-14 00:00:00') TO ('2023-01-15 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_15 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-15 00:00:00') TO ('2023-01-16 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_16 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-16 00:00:00') TO ('2023-01-17 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_17 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-17 00:00:00') TO ('2023-01-18 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_18 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-18 00:00:00') TO ('2023-01-19 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_19 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-19 00:00:00') TO ('2023-01-20 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_20 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-20 00:00:00') TO ('2023-01-21 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_21 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-21 00:00:00') TO ('2023-01-22 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_22 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-22 00:00:00') TO ('2023-01-23 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_23 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-23 00:00:00') TO ('2023-01-24 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_24 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-24 00:00:00') TO ('2023-01-25 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_25 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-25 00:00:00') TO ('2023-01-26 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_26 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-26 00:00:00') TO ('2023-01-27 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_27 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-27 00:00:00') TO ('2023-01-28 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_28 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-28 00:00:00') TO ('2023-01-29 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_29 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-29 00:00:00') TO ('2023-01-30 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_30 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-30 00:00:00') TO ('2023-01-31 00:00:00');
-    CREATE TABLE IF NOT EXISTS {targetTable}_31 PARTITION OF {targetTable} FOR VALUES FROM ('2023-01-31 00:00:00') TO ('2023-02-01 00:00:00');"""
 
-create_table = PostgresOperator(
-    task_id="create_table",
-    sql=createCommand,
-    dag=dag
-)
-
-#################################################################################### TABLE Truncate
-tuncateCommand=f"TRUNCATE TABLE {targetTable};"
-truncate_table = PostgresOperator(
-    task_id="truncate_table",
-    sql=tuncateCommand,
-    dag=dag
-)
-
-
-#################################################################################### FILE Transformation
+#################################################################################### FILE Transformation Task1
 def file_transform():
     data_files = os.listdir(sourcePath)
     data_files.sort()
@@ -122,26 +67,56 @@ transformation = PythonOperator(
     dag=dag
 )
 
-#################################################################################### Loading state
+
+#################################################################################### TABLE Creation Task2
+createCommand=f"""CREATE TABLE IF NOT EXISTS {targetTable} (
+    department_name VARCHAR(32),
+    sensor_serial VARCHAR(64),
+    create_at TIMESTAMP,
+    product_name VARCHAR(16),
+    product_expire TIMESTAMP)"""
+
+create_table = PostgresOperator(
+    task_id="create_table",
+    sql=createCommand,
+    dag=dag
+)
+
+#################################################################################### TABLE Truncate Task3
+tuncateCommand=f"TRUNCATE TABLE {targetTable};"
+truncate_table = PostgresOperator(
+    task_id="truncate_table",
+    sql=tuncateCommand,
+    dag=dag
+)
+
+
+
+
+#################################################################################### Loading state Task4
+connection : str = "postgres_default"
+pg_hook_load = PostgresHook(postgres_conn_id=connection)
+
 def data_loading():
     data_files = os.listdir(sourceTransPath)
     data_files.sort()
     total_ingest_file = 0
+
+    sql_command = f"""COPY {targetTable} (department_name, sensor_serial, create_at, product_name, product_expire)
+        FROM stdin WITH CSV HEADER
+        DELIMITER as ','
+    """
+
     for file_name in data_files:
-        DAY = file_name.split("-")[-1]
-        sql_command = f"""COPY {targetTable}_{DAY} (department_name, sensor_serial, create_at, product_name, product_expire)
-            FROM stdin WITH CSV HEADER
-            DELIMITER as ','
-        """
+
         file_path = f"{sourceTransPath}/{file_name}"
         # Open and execute the COPY command using the pg_hook
-        with open(file_path, 'r') as file:
-            pg_hook_load.copy_expert(sql=sql_command, filename=file_path)
+
+        pg_hook_load.copy_expert(sql=sql_command, filename=file_path)
         print(f"======================== `{file_name}` has loaded ========================")
         total_ingest_file += 1
 
     print(f"********************************** All files ingest SUCCEEDED  {total_ingest_file}**********************************")
-
 
 loading = PythonOperator(
     task_id='load_csv_into_table',
@@ -150,10 +125,7 @@ loading = PythonOperator(
 )
 
 
-
-
 #################################################################################### PIPELINE ####################################################################################
-
 
 start = DummyOperator(
     task_id='start',
@@ -164,4 +136,5 @@ end = DummyOperator(
     task_id='end',
     dag=dag,
 )
-start >> create_table >> truncate_table >> transformation >> loading >> end
+
+start >> transformation >> create_table >> truncate_table >> loading >> end
